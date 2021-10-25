1c853f4e0dbf9d6f15df883143294194771107cd
gouveialcc <gouveialcc@gmail.com>
2021-10-25 10:36:49 -0300

    ATT 25/10/2021
    AJUSTE DOS NOMES DE VARIÁVEIS E MENSAGENS DE TRATAMENTO

--- buscaPreco.py
+++ buscaPreco.py
@@ -22,29 +22,28 @@ locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 # VARIÁVEIS GLOBAIS
 url = 'https://www.mercadolivre.com.br/microsoft-xbox-one-s-1tb-standard-cor-branco/p/MLB14114827#reco_item_pos=1&reco_backend=machinalis-v2p-pdp-boost-v2&reco_backend_type=low_level&reco_client=vip-v2p&reco_id=6547dfe3-a60a-42a0-9c13-94f9b5a07f58'
-prcDsj = 2.900
-prcDsjCnv = str(prcDsj)
+prcDsj = 2.100
 
 # FUNCAO REQUEST E SOUP SITE MERCADO LIVRE
 def request():
-    global v1ml, v2ml, v2ml_value
-    siteml = requests.get(url)
-    print(siteml.status_code, siteml.ok)
+    global dscPrdt, valPrdt, valPrdtCnv
+    site = requests.get(url)
+    print(site.status_code, site.ok)
 
     # MONTANDO O SOUP ML
-    soupml = BeautifulSoup(siteml.content, 'html.parser')
-    v1ml = soupml.find('h1', class_='ui-pdp-title').get_text()
-    v2ml = soupml.find('span', class_='price-tag-fraction').get_text()
+    soup = BeautifulSoup(site.content, 'html.parser')
+    dscPrdt = soup.find('h1', class_='ui-pdp-title').get_text()
+    valPrdt = soup.find('span', class_='price-tag-fraction').get_text()
 
     # CONVERTE O VALOR RETORNADO DA VARIÁVEL DO PRECO ACIMA
-    v2ml_value = float(v2ml)
-    print(f'>>> DESCRIÇÃO DO PRODUTO: {v1ml}.')
-    print(f'>>> PREÇO: R${v2ml_value}.')
+    valPrdtCnv = float(valPrdt)
+    print(f'>>> DESCRIÇÃO DO PRODUTO: {dscPrdt}.')
+    print(f'>>> PREÇO: R${valPrdtCnv}.')
 
 # FUNCAO ENVIA MENSAGEM WHATSAPP
 def envMensagem():
     global contatos, x
-    msgPrcAlcancado = (f"PREÇO ALCANÇADO PARA O PRODUTO: {v1ml}. AGORA ESTÁ NO VALOR DE R${v2ml_value}.")
+    msgPrcAlcancado = (f"PREÇO ALCANÇADO PARA O PRODUTO: {dscPrdt}. AGORA ESTÁ NO VALOR DE R${valPrdtCnv}.")
     contatos = ['+5591991440764', '+5591991656408', '+5591980545716']
     x = 0
 
@@ -58,15 +57,15 @@ def envMensagem():
 request()
 
 # CONDICAO (PREÇO ALCANÇADO)
-if(prcDsj <= v2ml_value):
-    print(f'>>> PREÇO ALCANÇADO PARA O PRODUTO: {v1ml}, ESTÁ NO VALOR DE R${v2ml_value}.')
+if(prcDsj <= valPrdtCnv):
+    print(f'>>> PREÇO ALCANÇADO PARA O PRODUTO: {dscPrdt}, ESTÁ NO VALOR DE R${valPrdtCnv}.')
     envMensagem()
 else:
-    print(f'>>> PREÇO NÃO ALCANÇADO! O VALOR ATUAL É R${v2ml_value}.')
+    print(f'>>> PREÇO NÃO ALCANÇADO! O VALOR ATUAL É R${valPrdtCnv}.')
 
 # REPETE O REQUEST ATÉ ALCANCAR O PRECO DESEJADO
-while prcDsj > v2ml_value:
-    print(f'>>> PREÇO NÃO ALCANÇADO PARA O PRODUTO: {v1ml}.')
-    print('>>> NOVO REQUEST SERÁ FEITO EM INSTANTES.')
+while prcDsj > valPrdtCnv:
+    print(f'>>> PREÇO NÃO ALCANÇADO PARA O PRODUTO: {dscPrdt}.')
+    print('>>> NOVO REQUEST SERÁ REALIZADO.')
     time.sleep(5)
     request()
